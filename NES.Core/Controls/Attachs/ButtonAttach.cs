ea2a87136324619a1b59c4b627a58efa77dc2328
wyh <93712018+2236721325@users.noreply.github.com>
2023-04-01 16:05:55 +0800

    add button

--- NES.Core/Controls/Attachs/ButtonAttach.cs
+++ NES.Core/Controls/Attachs/ButtonAttach.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
 
 namespace NES.Core.Controls.Attachs
 {
     public class ButtonAttach
     {
+
+
+        public static double GetEnterShadow(DependencyObject obj)
+        {
+            return (double)obj.GetValue(EnterShadowProperty);
+        }
+
+        public static void SetEnterShadow(DependencyObject obj, double value)
+        {
+            obj.SetValue(EnterShadowProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for EnterShadow.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EnterShadowProperty =
+            DependencyProperty.RegisterAttached("EnterShadow", typeof(double), typeof(ButtonAttach), new PropertyMetadata(6.0));
+
+
+
 
 
 
