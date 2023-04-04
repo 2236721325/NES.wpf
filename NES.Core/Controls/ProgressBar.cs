7ff8e59c43cc00f9afbeb4e651944de958dd6ef7
wyh <93712018+2236721325@users.noreply.github.com>
2023-04-04 20:56:04 +0800

    Add progress bar control

---
+++ NES.Core/Controls/ProgressBar.cs
@@ -0,0 +1,20 @@
+﻿using NES.Core.Utilities.Draws;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace NES.Core.Controls
+{
+    public class ProgressBar:System.Windows.Controls.ProgressBar
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            var pen = new Pen(BorderBrush, (BorderThickness.Top + BorderThickness.Left + BorderThickness.Right + BorderThickness.Bottom) / 4);
+            DrawHelper.DrawPixRectangle(drawingContext, pen, ActualWidth, ActualHeight);
+        }
+    }
+}
