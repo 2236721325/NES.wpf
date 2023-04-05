97b216cd4768f25c708d0dfcf592e9ad1f53c845
wyh <93712018+2236721325@users.noreply.github.com>
2023-04-05 14:22:31 +0800

    add radio button

---
+++ NES.Core/Controls/RadioButton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NES.Core.Controls
+{
+    public class RadioButton: System.Windows.Controls.RadioButton
+    {
+   
+
+    }
+}
