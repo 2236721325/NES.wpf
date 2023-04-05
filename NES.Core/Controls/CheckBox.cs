fbff2e59121cecbc9fbb71dc637dfef4f455512c
wyh <93712018+2236721325@users.noreply.github.com>
2023-04-05 17:03:34 +0800

    Add CheckBox

--- NES.Core/Controls/CheckBox.cs
+++ NES.Core/Controls/CheckBox.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
 
 namespace NES.Core.Controls
 {
     public class CheckBox:System.Windows.Controls.CheckBox
     {
+
+
+        public double MarkHeight
+        {
+            get { return (double)GetValue(MarkHeightProperty); }
+            set { SetValue(MarkHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MarkHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MarkHeightProperty =
+            DependencyProperty.Register("MarkHeight", typeof(double), typeof(CheckBox));
+
 
     }
 }
